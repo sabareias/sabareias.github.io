0b8bb48da04558eba2ccc42bb8c13a7f50cf3d3a
Sabrina Areias <sabareias@gmail.com>
2018-12-19 17:10:07 -0500

    Final Project
    Submits Final

---
+++ final/js/scripts.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+
+$(".1").click(function(){
+  $(".2a").removeClass('hide');
+  $(".quiz1").addClass('hide');
+});
+
+$(".2").click(function(){
+  $(".2b").removeClass('hide');
+  $(".quiz1").addClass('hide');
+});
+
+$(".3").click(function(){
+  $(".2c").removeClass('hide');
+  $(".quiz1").addClass('hide');
+});
+
+$(".4").click(function(){
+  $(".3a").removeClass('hide');
+  $(".2a").addClass('hide');
+});
+
+$(".5").click(function(){
+  $(".3b").removeClass('hide');
+  $(".2a").addClass('hide');
+});
+
+$(".6").click(function(){
+  $(".3c").removeClass('hide');
+  $(".2a").addClass('hide');
+});
+
+$(".7").click(function(){
+  $(".3a").removeClass('hide');
+  $(".2b").addClass('hide');
+});
+
+$(".8").click(function(){
+  $(".3b").removeClass('hide');
+  $(".2b").addClass('hide');
+});
+
+$(".9").click(function(){
+  $(".3c").removeClass('hide');
+  $(".2b").addClass('hide');
+});
+
+$(".10").click(function(){
+  $(".3a").removeClass('hide');
+  $(".2c").addClass('hide');
+});
+
+$(".11").click(function(){
+  $(".3b").removeClass('hide');
+  $(".2c").addClass('hide');
+});
+
+$(".12").click(function(){
+  $(".3c").removeClass('hide');
+  $(".2c").addClass('hide');
+});
+
+
+
+});
