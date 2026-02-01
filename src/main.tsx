20a816b7e4f1f8d9c03c542756fd85765d3b354f
sabareias <sabareias@gmail.com>
2026-02-01 17:34:41 -0500

    removed index.css import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@
 import "/scss/custom.scss";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import Index from "./pages/Index";
 
 createRoot(document.getElementById('root')!).render(
