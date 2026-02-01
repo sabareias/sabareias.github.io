533b25a03d54d1e96b5e8d69d78a498a0af43757
sabareias <sabareias@gmail.com>
2026-02-01 17:18:00 -0500

    changed placement of GitHub button

--- src/components/Work/ProjectCard.tsx
+++ src/components/Work/ProjectCard.tsx
@@ -2,6 +2,7 @@ import { type FC, type ReactElement } from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import Stack from 'react-bootstrap/Stack';
+import { FiGithub } from 'react-icons/fi'
 
 interface ProjectCardProps {
     name: string
@@ -17,23 +18,26 @@ interface ProjectCardProps {
 const ProjectCard: FC<ProjectCardProps> = props => {
     return (
         <Card className="mb-4 shadow">
+            <a href={props.headerLink} aria-label={`Visit ${props.name} on GitHub`}>
             <Card.Img
             variant="top" 
             src={props.imageSrc} 
             className="rounded img-thumbnail"
-            />
+            alt={`Preview image of ${props.name} project`}
+            /></a>
             <Card.Body>
-                {props.links}
+                
                 <Card.Title className="fw-bold">{props.name}</Card.Title>
-                <Card.Subtitle className="text-muted mb-3 text-uppercase">{props.type}</Card.Subtitle>
+                <Card.Subtitle className="text-muted  mb-3 text-uppercase">{props.type}</Card.Subtitle>
                 <Stack direction="horizontal" gap={2} className="mb-3 justify-content-center">
                     {props.tags}
                 </Stack>
-                <Card.Text>
+                <Card.Text className="text-muted">
                 {props.description}
                 </Card.Text>
                 
                 <Button variant="primary" aria-label={`Read more about ${props.name}`} href={`#/projects/${props.id}`}>Read More</Button>
+                <Button variant="outline-secondary" aria-label={`Visit ${props.name} on GitHub`} href={props.headerLink} className="ms-2"><FiGithub /></Button>
             </Card.Body>
         </Card>
     )
