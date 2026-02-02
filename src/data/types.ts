46db9100e3afb1fafce6c12de70309ad7ace8c25
sabareias <sabareias@gmail.com>
2026-02-02 13:15:34 -0500

    added file to store interfaces/types

---
+++ src/data/types.ts
@@ -0,0 +1,18 @@
+import { type ReactElement } from 'react';
+
+export interface LinkObject {
+    link: string;
+    icon: ReactElement;
+}
+
+export interface ProjectObject {
+    name: string;
+    type: string;
+    id: string;
+    tags: Array<ReactElement>;
+    imageSrc: string;
+    tagline: string;
+    description: string;
+    links?: Array<LinkObject>;
+    headerLink: string;
+}
