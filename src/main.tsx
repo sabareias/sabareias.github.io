5c3ea0b1025b687bd86410c00b9f23c4bb5f2263
sabareias <sabareias@gmail.com>
2026-02-02 20:36:26 -0500

    implemented router

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,31 @@
 import "/scss/custom.scss";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; 
+
 import Index from "./pages/Index";
+import ProjectsPage from "./pages/Projects/ProjectsPage";
+import ProjectPage from "./pages/Project/ProjectPage";
+import NotFound from "./pages/NotFound";
+
+const router = createBrowserRouter([
+  {
+  path: "/",
+  element: <Index />,
+  errorElement: <NotFound />,
+  },
+  {
+    path: "/projects",
+    element: <ProjectsPage />,
+  },
+  {
+    path: "/projects/:projectId",
+    element: <ProjectPage />,
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Index />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
