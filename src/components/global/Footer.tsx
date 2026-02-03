35058b7b39aa8712ace39dccb191d1fd67a741f9
sabareias <sabareias@gmail.com>
2026-02-02 20:35:53 -0500

    added toggle theme button to footer

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -10,6 +10,27 @@ const scrollOptions = {
     smooth: true,
 };
 
+// toggles light vs dark mode theme
+    function toggleTheme(event: React.MouseEvent<HTMLButtonElement>) {
+        const htmlElement = document.querySelector("html");
+        const themeToggleIcon = event.currentTarget.querySelector(".theme-toggle-icon");
+        // confirm htmlElement is not null
+        if (htmlElement) {
+            const currentTheme = htmlElement.getAttribute("data-bs-theme");
+            let newTheme;
+            if (currentTheme === "light") {
+                themeToggleIcon?.classList.remove("bi-sun-fill");
+                themeToggleIcon?.classList.add("bi-moon-fill");
+                newTheme = "dark";
+            } else {
+                themeToggleIcon?.classList.remove("bi-moon-fill");
+                themeToggleIcon?.classList.add("bi-sun-fill");
+                newTheme = "light";
+            }
+            htmlElement.setAttribute("data-bs-theme", newTheme);
+        }
+    }
+
 // footer component
 function Footer() {
     return (
@@ -23,6 +44,8 @@ function Footer() {
                 <div className="d-flex justify-content-center align-items-middle gap-3">
                     <Socials /> 
                     <Button id="toTop" variant="outline-secondary" className="rounded-circle" onClick={() => animateScroll.scrollToTop(scrollOptions)}><SlArrowUp /></Button>
+                    <Button variant="outline-info" onClick={toggleTheme} className="me-2"><i className="bi bi-moon-fill theme-toggle-icon"></i></Button>
+                    
                 </div>
                     <p className="mt-3 text-muted">&copy; {new Date().getFullYear()} Sabrina Areias</p>
             
