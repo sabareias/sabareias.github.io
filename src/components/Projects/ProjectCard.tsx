d23eb3fc4e29a6dcb7739f0365d5003ba8011f7f
sabareias <sabareias@gmail.com>
2026-03-04 19:11:36 -0500

    Add button for live projects

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,23 +1,13 @@
-import { type FC, type ReactElement } from 'react'
+import { type FC} from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import Stack from 'react-bootstrap/Stack';
 import { FiGithub } from 'react-icons/fi'
-
-interface ProjectCardProps {
-    name: string
-    type: string
-    id: string
-    tags?: Array<ReactElement>
-    imageSrc: string
-    videoSrc?: string
-    description: string
-    links?: Array<ReactElement>
-    headerLink: string
-}
+import { type ProjectObject } from '../../data/types'
 
 // Project card component
-const ProjectCard: FC<ProjectCardProps> = props => {
+const ProjectCard: FC<ProjectObject> = props => {
+    const liveLink = props.links?.find((link) => link.linkType === 'live');
     return (
         <Card className="mb-4 shadow">
             <a href={props.headerLink} aria-label={`Visit ${props.name} on GitHub`} target="_blank">
@@ -40,6 +30,9 @@ const ProjectCard: FC<ProjectCardProps> = props => {
                 
                 <Button variant="primary" aria-label={`Read more about ${props.name}`} href={`#/projects/${props.id}`}>Read More</Button>
                 <Button variant="outline-secondary" aria-label={`Visit ${props.name} on GitHub`} href={props.headerLink} className="ms-2" target="_blank"><FiGithub /></Button>
+                {liveLink && (
+                    <Button variant="outline-secondary" aria-label={`Visit ${props.name} live`} href={liveLink.link} target="_blank" className='ms-2'>{liveLink.icon}</Button>
+                )}
             </Card.Body>
         </Card>
     )
