c862edbc8e679af541f2ce5775e44696590823d9
sabareias <sabareias@gmail.com>
2026-03-06 22:29:34 -0500

    Fix resume button

--- src/components/global/ResumeButton.tsx
+++ src/components/global/ResumeButton.tsx
@@ -1,11 +1,12 @@
 import Button from 'react-bootstrap/esm/Button';
+import resumePDF from '../../assets/SabrinaAreias_Resume.pdf';
 import { FiArrowUpRight } from "react-icons/fi";
 
 {/* open resume (new tab) button */}
 function ResumeButton() {
     return (
         <>
-        <a href="assets/SabrinaAreias_Resume-CG0aBAXK.pdf" target="_blank" rel="noreferrer">
+        <a target="_blank" href={resumePDF} >
             <Button variant="outline-secondary" className="mt-4">
                 Resume&ensp;<FiArrowUpRight />
             </Button>
